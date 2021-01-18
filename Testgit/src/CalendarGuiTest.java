dfbd7581e560f955d8aa5c756600913a022427d6
김용우 <@DESKTOP-TF6HNBN>
2021-01-18 23:14:50 +0900

    01-18 layout 완성

--- Testgit/src/CalendarGuiTest.java
+++ Testgit/src/CalendarGuiTest.java
@@ -1,6 +1,9 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Calendar;
 
 import javax.swing.DefaultComboBoxModel;
@@ -10,7 +13,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class CalendarGuiTest extends JFrame{
+public class CalendarGuiTest extends JFrame implements ActionListener{
 	Calendar now = Calendar.getInstance();
 		
 	JPanel selPane = new JPanel();
@@ -28,20 +31,23 @@ public class CalendarGuiTest extends JFrame{
 		JPanel northPane = new JPanel(new GridLayout(0,7));
 		JPanel centerPane = new JPanel(new GridLayout(0,7));
 	
-	String[] lblStr = {"일", "월", "화", "수", "목", "금", "토","1","2","3","4","5","6","7",
+	String[] lblStr = {"일", "월", "화", "수", "목", "금", "토"};
+	String[] lblNum = {"1","2","3","4","5","6","7",
 				"8","9","10","11","12","13","14","15","16","17","18","19","20","21",
 				"22","23","24","25","26","27","28","29","30","31"};
+	Font f = new Font("바탕", Font.BOLD, 20);
 	
-	
-	
-	
-	
+	int year;
+	int month;
+	int lastDay;
+	int space;
 	
 	public CalendarGuiTest() {
 		super("계산기");
-		
-		int year = now.get(Calendar.YEAR);
-		int month = now.get(Calendar.MONTH);
+		year = now.get(Calendar.YEAR);
+		month = now.get(Calendar.MONTH);
+		now.set(year, month, 1);
+		lastDay = now.getActualMaximum(Calendar.DAY_OF_MONTH);
 		
 		selPane.setBackground(Color.cyan);
 		selPane.add(before);
@@ -68,29 +74,86 @@ public class CalendarGuiTest extends JFrame{
 		
 		add(bigPane, BorderLayout.CENTER);
 		bigPane.add(northPane, BorderLayout.NORTH);
-			for(int i = 0; i<7; i++) {
+			for(int i = 0; i<lblStr.length; i++) {
 				JLabel weekName = new JLabel(lblStr[i], JLabel.CENTER);
 				northPane.add(weekName);
+				if(i == 0) {
+					weekName.setForeground(Color.red);
+				}else if(i==6) {
+					weekName.setForeground(Color.blue);
+				}
+				
+				
 			}
 		
 		bigPane.add(centerPane, BorderLayout.CENTER);
 		
-		int space = now.get(Calendar.DAY_OF_WEEK);
+		space = now.get(Calendar.DAY_OF_WEEK);
 		for(int empty = 1; empty <space; empty++) {
 			JLabel emptyRoom = new JLabel("", JLabel.CENTER);
 			centerPane.add(emptyRoom);
+		}
+		for(int i = 0; i<=lblNum.length-1; i++) {
+			JLabel calNum = new JLabel(lblNum[i], JLabel.CENTER);
+			centerPane.add(calNum);
+			if((space + i) % 7 == 0) {
+				calNum.setForeground(Color.blue);
+			}else if( (space+i) % 7 == 1){
+				calNum.setForeground(Color.red);
+			}
 		}
 		
 		
 		
 		
+		setSize(300,300);
+		setVisible(true);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		
-		
-		
+		before.addActionListener(this);
+		selYear.addActionListener(this);
+		selMonth.addActionListener(this);
+		after.addActionListener(this);
 		
 		
 	}
 
+	
+	public void actionPerformed(ActionEvent ae) {
+		Object eventObj = ae.getSource();
+		
+		if(eventObj instanceof JButton) {
+			if(eventObj == before) {
+				
+			}else if(eventObj == after) {
+				
+			}
+		}
+		
+		else if(eventObj instanceof JComboBox) {
+			if(eventObj == selYear) {
+				changeYear();
+			}else if(eventObj == selMonth) {
+				changeMonth();
+			}
+		}
+	}
+	
+	
+	
+	public void setCalendar() {
+			
+	}
+	
+	public void changeYear() {
+		
+	}
+	public void changeMonth() {
+		
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		new CalendarGuiTest();
 	}
